e6853b6f4ca13943094d855265e8c78621282cb9
Thijs Bressers <thijs.bressers@asml.com>
2024-12-02 12:46:24 +0100

    Training works

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Description
 
-Implementation of "Efficient Training of Language Models to Fill in the Middle" (ref.: https://arxiv.org/abs/2207.14255), taken from https://medium.com/@rajabmondal97/pretraining-extending-pretraining-of-autoregressive-model-using-the-fill-in-the-middle-objective-cc04c8f01d77
+Implementation of "Efficient Training of Language Models to Fill in the Middle" (ref.: https://arxiv.org/abs/2207.14255)
 
+Based on: https://medium.com/@rajabmondal97/pretraining-extending-pretraining-of-autoregressive-model-using-the-fill-in-the-middle-objective-cc04c8f01d77
--- fim.py
+++ fim.py
@@ -1,13 +1,11 @@
 from datasets import Dataset, DatasetDict, load_dataset
 from torch.utils.data import DataLoader
-import pandas as pandas
+import pandas as pd
 import random, torch
-from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments, DataCollatorForLanguageModeling
+from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments, DataCollatorForLanguageModeling, BitsAndBytesConfig
+from peft import LoraConfig, get_peft_model, TaskType, prepare_model_for_kbit_training
 
-checkpoint = "bigcode/starcoderbase-1b"
-tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-model = AutoModelForCausalLM.from_pretrained(checkpoint)
-tokenizer.pad_token = tokenizer.eos_token
+model_id = "meta-llama/Llama-3.2-3B-Instruct"
 
 prefix_token = "<fim_prefix>"
 middle_token = "<fim_middle>"
@@ -18,7 +16,7 @@ chuck_length = context_length
 max_len = context_length
 
 ds_train = load_dataset("huggingface-course/codeparrot-ds-train", split="train")
-ds_valid = load_dataset("huggingface-course/codeparrot-ds-valid", split="valid")
+ds_valid = load_dataset("huggingface-course/codeparrot-ds-valid", split="validation")
 
 raw_datasets = DatasetDict(
     {
@@ -112,8 +110,8 @@ def join_transformed_chunk_docs(transformed_chunk_docs):
 
 def apply_context_level_fim(chunk):
     chunk_docs = chunk.split(eot_token)
-    join_transformed_chunk_docs = apply_fim_transformation(chunk_docs)
-    joined_transformed_chunk_docs = join_transformed_chunk_docs(join_transformed_chunk_docs)
+    join_transformed_chunk_doc = apply_fim_transformation(chunk_docs)
+    joined_transformed_chunk_docs = join_transformed_chunk_docs(join_transformed_chunk_doc)
 
     formatted_example = tokenizer(
         joined_transformed_chunk_docs,
@@ -122,7 +120,10 @@ def apply_context_level_fim(chunk):
         max_length=max_len,
     )
 
-    return {"input_ids": torch.tensor(formatted_example["input_ids"]).squeeze(0), "attention_mask": torch.tensor(formatted_example["attention_mask"]).squeeze(0)}
+    return {
+        "input_ids": torch.tensor(formatted_example["input_ids"]).unsqueeze(0),
+        "attention_mask": torch.tensor(formatted_example["attention_mask"]).unsqueeze(0)
+    }
 
 class FimDataset(Dataset):
     def __init__(self, data):
@@ -176,27 +177,58 @@ chunk_ds = chunk_dataset(raw_datasets, chuck_length)
 train_dataset = FimDataset(chunk_ds["train"])
 eval_dataset = FimDataset(chunk_ds["valid"])
 
+# Lora configuration
+peft_config = LoraConfig(
+    task_type=TaskType.CAUSAL_LM,
+    r=64,
+    target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
+    lora_dropout=0.1,
+    use_rslora=True
+)
+
+# Setup bits and bytes config
+bnb_config = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_use_double_quant=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.float16
+)
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+tokenizer.pad_token = tokenizer.eos_token
 data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+model = AutoModelForCausalLM.from_pretrained(model_id, device_map='auto', quantization_config=bnb_config)
+model.gradient_checkpointing_enable()
+model = prepare_model_for_kbit_training(model)
+model = get_peft_model(model, peft_config)
+
 training_args = TrainingArguments(
-    output_dir = r".\\output",
+    output_dir=f"./models/{model_id}",
+    logging_dir=f"./logging/{model_id}",
+    save_strategy="steps",
+    num_train_epochs=16,
+    save_steps=200,
+    do_eval=True,
+    eval_strategy="steps",
+    eval_steps=50,
+    save_total_limit=8,
+    learning_rate=5e-4,
     per_device_train_batch_size=1,
     per_device_eval_batch_size=1,
-    evaluation_strategy="steps",
-    eval_steps=20,
-    logging_steps=20,
-    gradient_accumulation_steps=2,
-    num_train_epochs=16,
-    adam_beta1=0.9,
-    adam_beta2=0.95,
-    adam_epsilon=1e-8,
+    bf16=False,
+    push_to_hub=False,
+    optim="paged_adamw_32bit",
+    max_grad_norm=0.3,
     weight_decay=0.1,
-    warmup_steps=100,
     lr_scheduler_type="cosine",
-    learning_rate=5e-5,
-    save_strategy="steps",
-    save_total_limit=8,
-    load_best_model_at_end=True,
-    save_steps=50,
+    logging_strategy="steps",
+    logging_steps=1,
+    warmup_ratio=0.03,
+    gradient_accumulation_steps=2,
+    gradient_checkpointing=True,
+    remove_unused_columns = False,
+    report_to="tensorboard"
 )
 
 trainer = CustomTrainer(
